a4eb3ac8a9f061e6ec43a814553394cec5d07694
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-11-06 09:00:10 -0300

    Adicionando exemplo de uso de conta corrente

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,19 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        Pessoa joao = new Pessoa("João", "111.111.111-01",
-                "solteiro", 2000f,
-                LocalDate.of(2000,1,20));
-        Pessoa maria = new Pessoa("Maria", "222.222.222-02",
-                "Casada", 3000f,
-                LocalDate.of(1993,2,6));
-        Pessoa jose = new Pessoa("José", "333.333.333-03");
+        ContaCorrente c1 = new ContaCorrente(1, 1, "João");
 
-        System.out.println(joao.getId());
-        System.out.println(maria.getId());
-        System.out.println(jose.getId());
+        c1.depositar(200);
+        if(c1.sacar(100)){
+            System.out.println("Saque realizado com sucesso!");
+        }
+        if(c1.sacar(1000)){
+            System.out.println("Saque realizado com sucesso!");
+        }else{
+            System.out.println("Falha");
+        }
+
+        System.out.println(c1.getSaldo());
+
     }
 }
