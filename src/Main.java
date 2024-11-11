25c47a97b5f6ad67617c5f29271c19247fcc0b81
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-11-11 11:02:09 -0300

    Adicionando método para adicionar aluno

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,17 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
+        Professor professor = new Professor(123,"João",
+                "Ciência da Computação");
+
+        Turma turma = new Turma(professor, "POO",
+                "2M345", "ADS", 2);
+
+        turma.adicionarAluno(new Aluno(1, "João",
+                LocalDate.of(2000,2,4)));
+        turma.adicionarAluno(new Aluno(2, "Maria",
+                LocalDate.of(2001, 3,12)));
+
 
     }
 
--- src/Turma.java
+++ src/Turma.java
@@ -6,15 +6,23 @@ public class Turma {
     private String horario;
     private String curso;
     private int sala;
+    private int contAlunos;
 
-    public Turma(Professor professor, Aluno[] alunos, String disciplina,
+    public Turma(Professor professor, String disciplina,
                  String horario, String curso, int sala) {
         this.professor = professor;
-        this.alunos = alunos;
+        alunos = new Aluno[40];
+        contAlunos = 0;
         this.disciplina = disciplina;
         this.horario = horario;
         this.curso = curso;
         this.sala = sala;
+    }
+
+    public boolean adicionarAluno(Aluno aluno) {
+        if(contAlunos >= alunos.length) return false;
+        alunos[contAlunos++] = aluno;
+        return true;
     }
 
 }
