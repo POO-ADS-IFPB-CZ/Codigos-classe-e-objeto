f9422006ad6fa0891c214c67d3b5bcf2cd8d31bd
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-11-13 08:26:57 -0300

    Adicionando exemplo da mega sena

---
+++ src/Jogo.java
@@ -0,0 +1,9 @@
+public class Jogo {
+
+    private int[] numeros;
+
+    public Jogo(int quantidade){
+        this.numeros = new int[quantidade];
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,6 @@
-import java.time.LocalDate;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
 
-//        System.out.println(Arrays.toString(array));
-//        System.out.println(array.length);
-
-//        array = Arrays.copyOfRange(array, 1,100);
-//        System.out.println(Arrays.toString(array));
-
-//        char array[] = new char[10];
-//        Arrays.fill(array, 1, 5, 'A');
-//        System.out.println(Arrays.toString(array));
-
-        String array[] = {"Pedro", "Ana", "Jefferson", "Maria", "João", "ana"};
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.binarySearch(array, "Jefferson"));
 
     }
 
