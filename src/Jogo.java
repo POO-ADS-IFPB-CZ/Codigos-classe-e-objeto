1ce2a683ed17467f4dac51de7862646ba54da42d
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-11-13 08:39:42 -0300

    Adicionando métodos para exemplo da mega sena

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,9 +1,15 @@
 public class Jogo {
 
     private int[] numeros;
+    private int numerosAdicionados;
 
     public Jogo(int quantidade){
         this.numeros = new int[quantidade];
+        numerosAdicionados = 0;
+    }
+
+    public boolean isCompleto(){
+        return numerosAdicionados == numeros.length;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe quantos números quer jogar:");
+        int numero = scanner.nextInt();
+        if(numero <6 || numero > 20){
+            System.out.println("ERRO: Informe um valor entre 6 e 20\n" +
+                    "O programa será fechado");
+            System.exit(0);
+        }
+
+        Jogo jogo = new Jogo(numero);
+
+        while (!jogo.isCompleto()){
+
+        }
 
     }
 
